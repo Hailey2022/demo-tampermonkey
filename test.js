6aac37c62f4750749500012ebd77762a44d09a21
Hailey2022 <96413516+Hailey2022@users.noreply.github.com>
2023-02-06 14:06:00 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        test
+// @author      hailey
+// @description description2023
+// @license     GPL version 3
+// @match       https://www.google.com/*
+// @grant       unsafeWindow
+// @grant       GM_setClipboard
+// @run-at      document-end
+// @version     1.0.0
+// ==/UserScript==
+
+alert("you are using gooooooooooooooooooooooooooooogle")
